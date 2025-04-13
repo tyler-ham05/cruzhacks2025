74b3f6436a24f07d6a9cd77eab92378266185279
tyler-ham05 <tyjham05@gmail.com>
2025-04-12 21:39:03 -0700

    updated manual fields

--- syllabi.AI/lib/backend_services.dart
+++ syllabi.AI/lib/backend_services.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 //import 'package:intl/intl.dart';
 
 
-pushToDataBase(String name, List<String>concepts,List<String>keyWords)async {
+pushToDataBase(String name, List<String>concepts,List<String>keyWords, dates)async {
   try {
     final FirebaseFirestore firestore = FirebaseFirestore.instance;
     final String user = FirebaseAuth.instance.currentUser!.uid;
@@ -13,7 +13,7 @@ pushToDataBase(String name, List<String>concepts,List<String>keyWords)async {
       "course_name": name,
       "concepts": concepts,
       "key_words": keyWords, 
-      "dates": [], 
+      "dates": dates, 
     };
 
     await firestore.collection(user).doc(name).set(data);
